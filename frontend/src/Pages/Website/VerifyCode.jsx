5f32b41ede146e8af08c86c730d6abfd170b33f6
shubhamsaini1303 <144662519+shubhamsaini1303@users.noreply.github.com>
2025-01-30 23:56:41 +0530

    Update VerifyCode.jsx

--- frontend/src/Pages/Website/VerifyCode.jsx
+++ frontend/src/Pages/Website/VerifyCode.jsx
@@ -50,7 +50,9 @@ const Verify = () => {
     <div className="h-screen flex items-center justify-center bg-gray-100">
       <ToastContainer position="top-right" autoClose={3000} />
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Verify Account</h2>
+             <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">Task Managements</h2>
+            <hr />
+        <h2 className="text-xl font-bold text-center text-gray-800 mb-6">Verify Account</h2>
 
         <form onSubmit={handleVerify}>
           {/* Code Input */}
