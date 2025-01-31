3a185ea290b656b411f967e2a03d554065e6a13f
shubhamsaini1303 <144662519+shubhamsaini1303@users.noreply.github.com>
2025-01-31 10:11:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,17 @@ configDotenv();
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL], 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+
 
 app.use(express.json());
 app.use(cookieparser());
